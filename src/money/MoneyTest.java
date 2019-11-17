623f0507797ba1b4de0f46177f59aac3a9cd89ca
kimura <chitosws@gmail.com>
2019-11-17 18:40:30 +0900

    dev 為替掛け算の実装エラー状態
    p.4-p.5 クラスがない状態

--- src/money/MoneyTest.java
+++ src/money/MoneyTest.java
@@ -1,5 +1,31 @@
+//TODO
+// $5 + 10 CHF = $10
+// $5 * 2 = $10
+// amountをprivateにする
+// Dollarの副作用どうする
+// Moneyの丸め処理をどうする
+
 package money;
 
-public class MoneyTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
+/*
+ * TDD練習
+ * 異なる通貨の為替レートに基づいて換算されているか確認する。
+ *
+ */
+public class MoneyTest {
+	/*
+	 * Dollarクラスがない
+	 * コンストラクタがない
+	 * timesメソッドがない
+	 * amountメソッドがない
+	 */
+	@Test
+	public void testMultiplication() {
+		Dollar five = new Dollar(5);
+		five.times(2);
+		assertEquals(10, five.amount);
+	}
 }
