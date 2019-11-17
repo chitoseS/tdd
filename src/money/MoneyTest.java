dba0d1cbc16f6a7ecf9f0a83044bb9f7f25cd04a
kimura <chitosws@gmail.com>
2019-11-17 19:38:19 +0900

    dev:p15まで実装
    TODOの$5 * 2 = $10とMoneyの丸め処理をどうするを処理

--- src/money/MoneyTest.java
+++ src/money/MoneyTest.java
@@ -1,9 +1,9 @@
 //TODO
-// $5 + 10 CHF = $10
-// $5 * 2 = $10
-// amountをprivateにする
-// Dollarの副作用どうする
-// Moneyの丸め処理をどうする
+// []$5 + 10 CHF = $10
+// [x]$5 * 2 = $10
+// []amountをprivateにする
+// []Dollarの副作用どうする
+// [x]Moneyの丸め処理をどうする
 
 package money;
 
@@ -12,20 +12,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 /*
  * TDD練習
- * 異なる通貨の為替レートに基づいて換算されているか確認する。
+ * 異なる通貨の為替レートに基づいて換算されているかテストする
  *
  */
 public class MoneyTest {
 	/*
-	 * Dollarクラスがない
-	 * コンストラクタがない
-	 * timesメソッドがない
-	 * amountメソッドがない
+	 * 副作用どうする
 	 */
 	@Test
 	public void testMultiplication() {
 		Dollar five = new Dollar(5);
-		five.times(2);
-		assertEquals(10, five.amount);
+		Dollar product = five.times(2);
+		assertEquals(10, product.amount);
+		product = five.times(3);
+		assertEquals(15, product.amount);
 	}
 }
